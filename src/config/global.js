497a6d56c8d6382fe57c1cedbf814c0e569bf660
Carlos Abarca <carlos.alejandro1998@icloud.com>
2025-12-03 22:29:06 -0300

    Se agrega imagenes de huevos y correcciones esteticas

---
+++ src/config/global.js
@@ -0,0 +1,6 @@
+export const WHATSAPP_NUMBER = "56989676647" // Número de WhatsApp sin + ni espacios
+export const WHATSAPP_MESSAGE = "Hola, me gustaría obtener más información sobre sus productos"
+export const EMAIL = "contacto@dcdistribuciones.cl"
+export const BUSINESS_HOURS = "Lunes a Sábado: 09:00 - 19:00"
+export const SITE_NAME = "DC Distribuciones"
+export const SITE_SLOGAN = "Abastecimiento Evolucionado, Confianza Asegurada"
